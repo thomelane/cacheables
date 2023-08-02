a70795c1035ab490ce1f6db4e9c59be8f7d69ce8
Thom Lane <thom.lane@clarity.ai>
2023-08-02 10:48:11 +0100

    Added versions metadata and listing

--- cacheables/core.py
+++ cacheables/core.py
@@ -1,10 +1,12 @@
 import contextlib
+import datetime
 import functools
 import hashlib
 import json
 import os
 import pickle
 import shutil
+import subprocess
 import sys
 import tempfile
 import warnings
@@ -120,33 +122,63 @@ class CacheableFunction:
         except Exception as error:
             raise VersionIdException(str(error)) from error
 
-    def get_path_from_inputs(self, *args, **kwargs) -> Path:
-        input_id = self._get_input_id(*args, **kwargs)
-        version_id = self._get_version_id()
+    def _construct_function_path(
+        self,
+        name: Optional[str] = None,
+        base_path: Optional[str] = None,
+    ) -> Path:
+        name = name or self._name
+        base_path = base_path or self._base_path
+        path = Path(base_path, "functions", name)
+        return path
+
+    def _construct_version_path(
+        self,
+        version_id: Optional[str] = None,
+        name: Optional[str] = None,
+        base_path: Optional[str] = None,
+    ) -> Path:
+        version_id = version_id or self._get_version_id()
         path = Path(
-            self._base_path,
-            "functions",
-            self._name,
-            "versions",
-            version_id,
-            "inputs",
-            input_id,
-            "outputs",
+            self._construct_function_path(name, base_path), "versions", version_id
         )
         return path
 
-    def get_path_from_ids(self, input_id: str, version_id: Optional[str]) -> Path:
-        version_id = version_id or self._get_version_id()
+    def _construct_input_path(
+        self,
+        input_id: str,
+        version_id: Optional[str] = None,
+        name: Optional[str] = None,
+        base_path: Optional[str] = None,
+    ) -> Path:
         path = Path(
-            self._base_path,
-            "functions",
-            self._name,
-            "versions",
-            version_id,
+            self._construct_version_path(version_id, name, base_path),
             "inputs",
             input_id,
-            "outputs",
         )
+        return path
+
+    def _construct_output_path(
+        self,
+        input_id: str,
+        version_id: Optional[str] = None,
+        name: Optional[str] = None,
+        base_path: Optional[str] = None,
+    ) -> Path:
+        path = Path(
+            self._construct_input_path(input_id, version_id, name, base_path), "outputs"
+        )
+        return path
+
+    def get_path_from_inputs(self, *args, **kwargs) -> Path:
+        input_id = self._get_input_id(*args, **kwargs)
+        path = self._construct_output_path(input_id=input_id)
+        return path
+
+    def get_path_from_ids(
+        self, input_id: str, version_id: Optional[str] = None
+    ) -> Path:
+        path = self._construct_output_path(input_id=input_id, version_id=version_id)
         return path
 
     def __call__(self, *args, **kwargs):
@@ -198,14 +230,16 @@ class CacheableFunction:
 
     @contextlib.contextmanager
     def enable_cache(self, read: bool = True, write: bool = True):
-        _read, _write = self._read, self._write
+        previous_read, previous_write = self._read, self._write
         # most restrictive settings will be used
-        self._read = (read and _read) if (_read is not None) else read
-        self._write = (write and _write) if (_write is not None) else write
+        self._read = (read and previous_read) if (previous_read is not None) else read
+        self._write = (
+            (write and previous_write) if (previous_write is not None) else write
+        )
         try:
             yield
         finally:
-            self._read, self._write = _read, _write
+            self._read, self._write = previous_read, previous_write
 
     @contextlib.contextmanager
     def disable_cache(self):
@@ -242,6 +276,7 @@ class CacheableFunction:
                 if path.exists() and path.is_dir():
                     shutil.rmtree(path)
                 shutil.copytree(tmp_path, path)
+                self._dump_version_metadata()
         except Exception as error:
             raise DumpException(str(error)) from error
 
@@ -251,18 +286,8 @@ class CacheableFunction:
             raise MissingResultException("result not found in cache")
         return self._read_from_cache(path)
 
-    def load_from_ids(self, input_id: str, version_id: Optional[str]):
-        version_id = version_id or self._get_version_id()
-        path = Path(
-            self._base_path,
-            "functions",
-            self._name,
-            "versions",
-            version_id,
-            "inputs",
-            input_id,
-            "outputs",
-        )
+    def load_from_ids(self, input_id: str, version_id: Optional[str] = None):
+        path = self._construct_output_path(input_id=input_id, version_id=version_id)
         if not path.exists():
             raise MissingResultException("result not found in cache")
         return self._read_from_cache(path)
@@ -272,6 +297,82 @@ class CacheableFunction:
             "input_id": self._get_input_id(*args, **kwargs),
             "version_id": self._get_version_id(),
         }
+
+    def _dump_version_metadata(self) -> None:
+        """
+        Save metadata.json for the current version (if it doesn't already exist)
+        Save it at `<base_path>/<name>/versions/<version_id>/metadata.json`
+        """
+        metadata_path = Path(self._construct_version_path(), "metadata.json")
+        if not metadata_path.exists():
+            output_metadata = {
+                "created_at": datetime.datetime.utcnow().isoformat() + "Z",
+                "version_id": self._get_version_id(),
+                "metadata": self._metadata,
+            }
+            try:
+                commit_hash = (
+                    subprocess.check_output(["git", "rev-parse", "HEAD"])
+                    .decode("utf-8")
+                    .strip()
+                )
+                # check if there are any uncommitted changes (clean=False if there are)
+                clean = (
+                    subprocess.check_output(["git", "status", "--porcelain"])
+                    .decode("utf-8")
+                    .strip()
+                    == ""
+                )
+                output_metadata["git"] = {"commit_hash": commit_hash, "clean": clean}
+            except subprocess.CalledProcessError as error:
+                self._logger.warning(f"failed to get git metadata: {error}")
+            metadata_path.parent.mkdir(parents=True, exist_ok=True)
+            with open(metadata_path, "w", encoding="utf-8") as f:
+                json.dump(output_metadata, f)
+
+    @staticmethod
+    def _get_directory_statistics(directory: Path) -> dict:
+        """
+        Get statistics for a given directory: number of subdirectories and total bytes.
+        """
+        if directory.exists():
+            return {
+                "count": len(list(directory.glob("*/"))),
+                "total_bytes": sum(
+                    f.stat().st_size for f in directory.glob("**/*") if f.is_file()
+                ),
+            }
+        else:
+            return {"count": 0, "total_bytes": 0}
+
+    @staticmethod
+    def _load_version_metadata(metadata_path: Path) -> dict:
+        """
+        Read version metadata from a JSON file.
+        """
+        return (
+            json.loads(metadata_path.read_text(encoding="utf-8"))
+            if metadata_path.exists()
+            else {}
+        )
+
+    def get_versions(self) -> list:
+        """
+        Loop through all versions in cache and return the metadata.json for each
+        i.e. `<base_path>/<name>/versions/*/metadata.json`
+        """
+        versions_path = Path(self._construct_function_path(), "versions").resolve(
+            strict=False
+        )
+        versions = [
+            {
+                **self._load_version_metadata(version_dir / "metadata.json"),
+                "statistics": self._get_directory_statistics(version_dir / "inputs"),
+            }
+            for version_dir in versions_path.glob("*/")
+        ]
+        versions.sort(key=lambda x: x["created_at"])
+        return versions
 
 
 # should be a classmethod, but `enable_cache` is an instance method
